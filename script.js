c9a5a7f91d9c65dfab84ec930ac0d65b558ca7f5
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2025-12-10 00:58:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-let EnteredButton = document.querySelector("button")
+let EnteredButton = document.querySelector("#enterBtn")
 let status = document.querySelector("#status")
 EnteredButton.addEventListener("click", () => {
 	status.innerHTML="<h1>Entered Metaverse</h1>"
