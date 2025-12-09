a5688cecbb18dacf33feea8111017622af2e328d
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2025-12-10 00:56:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 let EnteredButton = document.querySelector("button")
 let status = document.querySelector("#status")
 EnteredButton.addEventListener("click", () => {
-	status.innerHTML="<h1>Entered metaverse</h1>"
+	status.innerHTML="<h1>Entered Metaverse</h1>"
 })
